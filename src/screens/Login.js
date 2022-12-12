afad53b16c7284b7c2fd67466cd6646d37690dfd
Rohan Meshram <irohanmeshram@gmail.com>
2022-12-12 10:27:42 +0530

    Version 0.1

---
+++ src/screens/Login.js
@@ -0,0 +1,92 @@
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, StatusBar } from 'react-native'
+import React, {useState} from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const Login = () => {
+
+const [isIconVisible, setIsIconVisible] = useState(true)
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageBox}>
+        <Image source={{ uri: 'https://cdni.iconscout.com/illustration/premium/thumb/indian-food-5863587-4874941.png' }}
+          style={{ height: 200, width: 250 }} />
+        <Text style={{
+          padding: 15,
+          fontSize: 32,
+          fontFamily: 'RobotoSlab-Bold',
+          color: '#FFECD1',
+        }}>Admin Login</Text>
+      </View>
+
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.formBox}>
+
+            <View style={styles.customInput}>
+              <Ionicons name='mail-outline' size={20} color={'#001524'}/>
+              <TextInput placeholder='Email'
+                        keyboardType={'email-address'} style={{flex: 1, paddingLeft: 10, fontSize: 16, fontFamily: 'RobotoSlab-Regular'}}/>
+            </View>
+
+            <View style={styles.customInput}>
+            <Ionicons name='lock-closed-outline' size={20} color={'#001524'}/>
+              <TextInput placeholder='Password' 
+                         secureTextEntry={isIconVisible ? true : false} style={{paddingLeft: 10, flex: 1, fontSize: 16, fontFamily: 'RobotoSlab-Regular'}}/>
+              <TouchableOpacity onPress={() => setIsIconVisible(!isIconVisible)}>
+                <Ionicons name={isIconVisible ? 'eye-off-outline' : 'eye-outline'}
+                          size={20} color={'#001524'}  style={{ marginRight: 5 }}/>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={{alignItems: 'center',
+                                      backgroundColor: '#78290F',
+                                      padding: 15,
+                                      marginHorizontal: 80,
+                                      marginVertical: 20,
+                                      borderRadius: 10,
+                                      justifyContent: 'space-between'}}>
+              <Text style={{fontFamily: 'RobotoSlab-Regular', 
+                            color: '#FFECD1',
+                            fontSize: 18}}>Login</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#BA6144',
+  },
+  imageBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: 30
+  },
+  formBox: {
+  //  height: '50%',
+    width: '95%',
+    backgroundColor: 'rgba(127,127,127,0.5)',
+    borderRadius: 10,
+  },
+  customInput: {
+    backgroundColor: '#FFECD1',
+    marginHorizontal: 15,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    color: '#001524',
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+
+})
+
+
+
---
+++ src/screens/Splash.js
@@ -0,0 +1,73 @@
+import { View, Text, StyleSheet, Image, StatusBar } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect } from '@react-navigation/native'
+
+const Splash = () => {
+
+  useFocusEffect (() => {
+    StatusBar.setHidden(true)
+  })
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Login')
+     // StatusBar.setHidden(false)
+    }, 2500)
+  }, [])
+
+
+
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: 'https://cdni.iconscout.com/illustration/premium/thumb/indian-food-5863587-4874941.png' }}
+          style={{ height: 200, width: 250 }} />
+        <View style={styles.textContainer}>
+          <Text style={styles.inputText}>Food</Text>
+          <Text style={[styles.inputText, { marginTop: -13 }]}>App</Text>
+        </View>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <Text style={styles.bottomText}>Cooked with Perfection</Text>
+      </View>
+    </View>
+  )
+}
+
+export default Splash
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#78290F',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    alignItems: 'center',
+    // paddingBottom: 100,
+  //  marginBottom:
+  },
+  inputText: {
+    fontSize: 32,
+    fontFamily: 'RobotoSlab-Bold',
+    color: '#FFECD1',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginVertical: 180,
+    padding: 50
+  },
+  bottomContainer: {
+    alignItems: 'center'
+  },
+  bottomText: {
+    color: '#FFECD1',
+    fontSize: 16,
+    fontFamily: 'RobotoSlab-Regular'
+  }
+})
